40a128b0936b331666e859f970417db1cc98f1b7
nazia911 <nazia2409a@aptechorangi.com>
2025-12-26 13:33:58 -0800

    apna database ka kaam  bhi push kr rahi hun

--- backend/database/Lab_automation.sql
+++ backend/database/Lab_automation.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Dec 26, 2025 at 01:02 AM
+-- Host: 127.0.0.1
+-- Generation Time: Dec 26, 2025 at 10:22 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,16 +18,64 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Lab_automation`
+-- Database: `lab_automation`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `UserStaafe`
+-- Table structure for table `product`
 --
 
-CREATE TABLE `UserStaafe` (
+CREATE TABLE `product` (
+  `id` int(200) NOT NULL,
+  `product_code` varchar(200) NOT NULL,
+  `rivision` varchar(200) NOT NULL,
+  `manufacturing_number` int(200) NOT NULL,
+  `product_name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `product_code`, `rivision`, `manufacturing_number`, `product_name`) VALUES
+(1234567897, 'cd12', 'we12', 1234, 'swtichgear'),
+(1234567898, 'cd34', 'we23', 1235, 'capacitor'),
+(1234567899, 'fd23', 'we56', 1237, 'swtichgear'),
+(1234567900, 'cd12', 'we12', 1234, 'swtichgear'),
+(1234567901, 'cd12', 'we13', 1235, 'swtichgear');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `testing_data`
+--
+
+CREATE TABLE `testing_data` (
+  `testing_id` varchar(12) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `testing_type` varchar(200) DEFAULT NULL,
+  `result_type` varchar(200) DEFAULT NULL,
+  `tested_by` varchar(200) DEFAULT NULL,
+  `remarks` varchar(200) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `testing_data`
+--
+
+INSERT INTO `testing_data` (`testing_id`, `product_id`, `testing_type`, `result_type`, `tested_by`, `remarks`, `created_at`) VALUES
+('880IIZ261225', 1234567898, '3', '1', 'afsheen', 'sdcvsdgv', '2025-12-26 21:18:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userstaafe`
+--
+
+CREATE TABLE `userstaafe` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
@@ -37,10 +85,10 @@ CREATE TABLE `UserStaafe` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `UserStaafe`
+-- Dumping data for table `userstaafe`
 --
 
-INSERT INTO `UserStaafe` (`id`, `name`, `email`, `passwd`, `roll`, `image`) VALUES
+INSERT INTO `userstaafe` (`id`, `name`, `email`, `passwd`, `roll`, `image`) VALUES
 (1, 'DAWOOD', 'dawod2@gamil.com', 'dawood123', 'admin', '../images/1218911.jpg');
 
 --
@@ -48,9 +96,22 @@ INSERT INTO `UserStaafe` (`id`, `name`, `email`, `passwd`, `roll`, `image`) VALU
 --
 
 --
--- Indexes for table `UserStaafe`
+-- Indexes for table `product`
 --
-ALTER TABLE `UserStaafe`
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `testing_data`
+--
+ALTER TABLE `testing_data`
+  ADD UNIQUE KEY `testing_id` (`testing_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `userstaafe`
+--
+ALTER TABLE `userstaafe`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
@@ -59,10 +120,26 @@ ALTER TABLE `UserStaafe`
 --
 
 --
--- AUTO_INCREMENT for table `UserStaafe`
+-- AUTO_INCREMENT for table `product`
 --
-ALTER TABLE `UserStaafe`
+ALTER TABLE `product`
+  MODIFY `id` int(200) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1234567902;
+
+--
+-- AUTO_INCREMENT for table `userstaafe`
+--
+ALTER TABLE `userstaafe`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `testing_data`
+--
+ALTER TABLE `testing_data`
+  ADD CONSTRAINT `testing_data_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
