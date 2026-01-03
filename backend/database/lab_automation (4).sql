195c99eb5e32bfc14fba2a2d5d76372e0dfa1cb4
nazia911 <nazia2409a@aptechorangi.com>
2026-01-03 11:10:52 -0800

    All work heas done reports and graph are optional

--- backend/database/lab_automation (4).sql
+++ backend/database/lab_automation (4).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 03, 2026 at 03:33 PM
+-- Generation Time: Jan 03, 2026 at 08:08 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -43,23 +43,23 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `product_id`, `product_code`, `rivision`, `manufacturing_no`, `product_type`, `product_name`, `is_active`) VALUES
-(12, 'FSR20001', 'FS', 'R2', '0001', 'Fuse', 'Drop-Out Fuse', 1),
-(16, 'CPR10002', 'CP', 'R1', '0002', 'Capacitor', 'Three Phase Power Capacitor', 0),
+(12, 'FSR20001', 'FS', 'R2', '0001', 'Fuse', 'Drop-Out Fuse', 0),
+(16, 'CPR10002', 'CP', 'R1', '0002', 'Capacitor', 'Three Phase Power Capacitor', 1),
 (17, 'FSR10009', 'FS', 'R1', '0009', 'Fuse', 'Thermal Fuse', 0),
-(18, 'RSR20006', 'RS', 'R2', '0006', 'Resistor', 'Carbon Film Resistor 1kΩ', 1),
+(18, 'RSR20006', 'RS', 'R2', '0006', 'Resistor', 'Carbon Film Resistor 1kΩ', 0),
 (19, 'FSR10034', 'FS', 'R1', '0034', 'Fuse', 'Drop-Out Fuse', 1),
-(21, 'CPR10089', 'CP', 'R1', '0089', 'Capacitor', 'Three Phase Power Capacitor', 1),
-(25, 'SGR10010', 'SG', 'R1', '0010', 'Switchgear', '11kV Indoor Switchgear Panel', 1),
-(27, 'FSR10029', 'FS', 'R1', '0029', 'Fuse', 'HRC Fuse 32A', 1),
-(28, 'RSR20045', 'RS', 'R2', '0045', 'Resistor', 'Carbon Film Resistor 1kΩ', 1),
+(21, 'CPR10089', 'CP', 'R1', '0089', 'Capacitor', 'Three Phase Power Capacitor', 0),
+(25, 'SGR10010', 'SG', 'R1', '0010', 'Switchgear', '11kV Indoor Switchgear Panel', 0),
+(27, 'FSR10029', 'FS', 'R1', '0029', 'Fuse', 'HRC Fuse 32A', 0),
+(28, 'RSR20045', 'RS', 'R2', '0045', 'Resistor', 'Carbon Film Resistor 1kΩ', 0),
 (30, 'SGR10065', 'SG', 'R1', '0065', 'Switchgear', '11kV Indoor Switchgear Panel', 1),
 (33, 'RSR20074', 'RS', 'R2', '0074', 'Resistor', 'Carbon Film Resistor 1kΩ', 1),
 (34, 'FSR20123', 'FS', 'R2', '0123', 'Resistor', 'Cement Resistor 5W', 1),
-(38, 'SGR20087', 'SG', 'R2', '0087', 'Switchgear', '11kV Outdoor Switchgear Panel', 1),
-(39, 'RSR20019', 'RS', 'R2', '0019', 'Resistor', 'High Power Resistor 100W', 1),
+(38, 'SGR20087', 'SG', 'R2', '0087', 'Switchgear', '11kV Outdoor Switchgear Panel', 0),
+(39, 'RSR20019', 'RS', 'R2', '0019', 'Resistor', 'High Power Resistor 100W', 0),
 (41, 'CPR10088', 'CP', 'R1', '0088', 'Capacitor', 'LT Capacitor Bank Panel', 1),
 (42, 'FSR20889', 'FS', 'R2', '0889', 'Fuse', 'Kit-Kat Fuse', 1),
-(43, 'SGR10888', 'SG', 'R1', '0888', 'Switchgear', '11kV Indoor Switchgear Panel', 1),
+(43, 'SGR10888', 'SG', 'R1', '0888', 'Switchgear', '11kV Indoor Switchgear Panel', 0),
 (45, 'FSR10222', 'FS', 'R1', '0222', 'Fuse', 'Drop-Out Fuse', 1),
 (46, 'RSR10555', 'RS', 'R1', '0555', 'Resistor', 'Cement Resistor 5W', 1);
 
@@ -87,12 +87,22 @@ CREATE TABLE `testing_data` (
 --
 
 INSERT INTO `testing_data` (`testing_id`, `product_id`, `product_type`, `testing_type`, `result_type`, `tested_by`, `remarks`, `created_at`, `send_to`, `is_locked`) VALUES
+('2DP170030126', 43, 'Switchgear', 'Insulation Resistance', 'Fail', 'maheen', 'product men kharabhi he', '2026-01-03 16:56:01', 'Remanufacture', 0),
+('9JDH1U030126', 38, 'Switchgear', 'Current Test', 'Pending', 'maheen', 'abhi completely check nahi hua', '2026-01-03 16:55:12', 'Pending', 0),
 ('A75MDK020126', 12, 'Fuse', 'Voltage Test', 'Pass', 'dawood', 'theek ', '2026-01-02 21:05:37', 'CPRI', 1),
 ('C32K94010126', 16, 'Capacitor', 'Voltage Test', 'Pass', 'amir', 'thek ', '2026-01-01 16:39:43', 'CPRI', 1),
 ('E1OLBA010126', 12, 'Fuse', 'Voltage Test', 'Pass', 'dawood', 'correct', '2026-01-01 16:12:37', 'CPRI', 1),
-('F4TPVH010126', 17, 'Fuse', 'Insulation Resistance', 'Pending', 'noureen', 'abhi check nahi hua ', '2026-01-01 18:30:31', 'Pending', 0),
+('F4TPVH010126', 17, 'Fuse', 'Insulation Resistance', 'Pass', 'noureen', 'passs ho gaya', '2026-01-01 18:30:31', 'CPRI', 1),
 ('JHDPHF020126', 12, 'Fuse', 'Insulation Resistance', 'Pass', 'afsheen', 'sahii he', '2026-01-02 21:20:11', 'CPRI', 1),
-('WHO8Z8020126', 12, 'Fuse', 'Continuity Test', 'Pass', 'maheen', 'pass', '2026-01-02 21:22:39', 'CPRI', 1);
+('MB7BAP030126', 21, 'Capacitor', 'Voltage Test', 'Fail', 'Muhammad', 'theek nahi he', '2026-01-03 16:54:19', 'Remanufacture', 0),
+('NGWHKF030126', 30, 'Switchgear', 'Insulation Resistance', 'Pass', 'dawood', 'pass ho gya', '2026-01-03 17:00:33', 'CPRI', 1),
+('QKV0CS030126', 18, 'Resistor', 'Voltage Test', 'Pending', 'Nazia', 'abhi copletely chack nahi hua', '2026-01-03 16:58:29', 'Pending', 0),
+('TD0UEO030126', 27, 'Fuse', 'Voltage Test', 'Pass', 'dawood', 'theek he', '2026-01-03 15:12:25', 'CPRI', 1),
+('UYKLPM030126', 39, 'Resistor', 'Voltage Test', 'Pass', 'Raheem', 'pass ho gya', '2026-01-03 16:56:38', 'CPRI', 1),
+('WHO8Z8020126', 12, 'Fuse', 'Continuity Test', 'Pass', 'maheen', 'pass', '2026-01-02 21:22:39', 'CPRI', 1),
+('WO15J5030126', 12, 'Fuse', 'Insulation Resistance', 'Pass', 'afsheen', 'pass ho gya', '2026-01-03 15:14:03', 'CPRI', 1),
+('XOI1TO030126', 28, 'Resistor', 'Insulation Resistance', 'Pending', 'Rahmaan', 'abhi copletely chack nahi hua', '2026-01-03 16:57:41', 'Pending', 0),
+('Y020G0030126', 25, 'Switchgear', 'Current Test', 'Pass', 'dawood', 'theek he', '2026-01-03 15:11:44', 'CPRI', 1);
 
 -- --------------------------------------------------------
 
@@ -117,7 +127,8 @@ INSERT INTO `userstaafe` (`id`, `name`, `email`, `passwd`, `roll`, `image`) VALU
 (2, 'DAWOOD', 'dawod2@gamil.com', '$2y$10$fOSfzZsw7OUlzOHlqxN7kOXSBt8pB.ZPy59siyN9JpaMxkHN89saO', 'admin', 'qwqwq.jpg'),
 (4, 'Ali', 'Alikhattak@gmail.com', '$2y$10$OcRK0d6WwdhPp2ITRgdyH.2wlaz5gzSsMhprtB2GvqRGEGxeY5x6e', 'user', 'ZMmAKg.png'),
 (5, 'Bint e Nasir', 'Nasir@gmail.com', '$2y$10$7xPIFtB8LOhQ9BduNSE48e05q4mJ3Yut7J2jbffCez.hGbLvY7CsO', 'admin', 'admin.jpg'),
-(6, 'Bint e Nasir', 'sheen@gmail.com', '$2y$10$jWb59jK.C64AGuzly1XDv.jFACYg16umamwfmIEntRHiiJ1cZ5CF2', 'admin', 'admin.jpg');
+(6, 'Bint e Nasir', 'sheen@gmail.com', '$2y$10$jWb59jK.C64AGuzly1XDv.jFACYg16umamwfmIEntRHiiJ1cZ5CF2', 'admin', 'admin.jpg'),
+(7, 'nazia', 'naz@gmail.com', '$2y$10$gRpZrXMsS.tn/VN.91TXq.D2rHKYVB5lMlBV0PHJfovPcRyF.Qk4u', 'admin', 'naz2.png');
 
 --
 -- Indexes for dumped tables
@@ -158,7 +169,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `userstaafe`
 --
 ALTER TABLE `userstaafe`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
---
+++ view-product.php
@@ -0,0 +1,78 @@
+<?php
+include 'header.php';
+include "backend/db.php";
+// table join query///
+$id = $_GET['id'];
+
+$q = "SELECT * FROM products WHERE id = '$id'";
+
+$result = mysqli_query($conn, $q);
+
+// if (!$result) {
+//     die("Query Failed: " . mysqli_error($conn));
+// }
+
+
+$data = mysqli_fetch_assoc($result);
+
+
+
+// echo '<pre>';
+// echo print_r($data);
+?>
+
+        <!-- App body starts -->
+        <div class="app-body">
+            <div class="container-fluid justify-content-center align-items-center">
+        <div class="row  justify-content-center align-items-center">
+        <div class="col-10 m-auto">
+                <div class="card mb-4">
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col-sm-6">
+                        <img src="forntend/switchgear/05.jpg" class="img-fluid rounded-2" alt="Bootstrap Gallery" />
+                      </div>
+                      <div class="col-sm-6">
+                        <div class="align-items-center">
+                          <h5 class="clr mt-3">Product Type : <?= $data['product_type'] ?></h5>
+                          <h5 class="clr">Product Name : <?= $data['product_name'] ?></h5>
+                          <h5 class="clr">Product ID : <?= $data['product_id'] ?></h5>
+                          <h5 class="clr">Manufacturing No : <?= $data['manufacturing_no'] ?></h5>
+                          <h5 class="clr">Product Code : <?= $data['product_code'] ?></h5>
+                          <h5 class="clr">Revision : <?= $data['rivision'] ?></h5>
+                        <?php 
+                         if($data['is_active'] == 1){
+    // ACTIVE PRODUCT
+    ?>
+   
+    <a href="edit.php?id=<?= $data['id']?>" class="btn btn-warning btn-sm">Update</a>
+    <a href="backend/deactivate-product.php?id=<?= $data['id']?>"
+    class="btn btn-danger btn-sm"
+    onclick="return confirm('Deactivate this product?')">Deactivate</a>
+    <a href="products-list.php?id=<?= $data['id']?>" class="btn btn-dark btn-sm">Back </a>
+    
+    <?php
+}else{
+    // INACTIVE PRODUCT
+    ?>
+   
+   <a href="view-product.php?id=<?= $data['id']?>" class="btn btn-info btn-sm">View</a>
+   <a href="backend/reactivate-product.php?id=<?= $data['id']?>" 
+   class="btn btn-success btn-sm">Reactivate</a>
+   <a href="products-list.php?id=<?= $data['id']?>" class="btn btn-dark btn-sm">Back </a>
+      
+    <?php
+}
+?>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </div>
+              </div>
+
+        </div>
+        <?php
+        include 'footer.php';
