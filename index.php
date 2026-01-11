ee7008189f5237ed250e23a240229240e54629a8
DAWOOD-KTK <dawood2411d@gmail.com>
2026-01-11 14:13:45 +0500

    done

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['webuser'])) {
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-	<title>Electro - HTML Ecommerce Template</title>
+	<title>SRS Lab automations</title>
 
 
 	<!-- Google font -->
